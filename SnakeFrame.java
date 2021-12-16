5e77f95d2d71a8f2f338d6b1182391ec1c05f219
jillianyen <78186234+jillianyen@users.noreply.github.com>
2021-12-15 19:30:06 -0600

    Update SnakeFrame.java

--- SnakeFrame.java
+++ SnakeFrame.java
@@ -1,21 +1,20 @@
 package snake2;
-
 import java.awt.event.*;
 import javax.swing.JFrame;
 
-public class SnakeFrame extends JFrame //implements KeyListener
+public class SnakeFrame extends JFrame
 {
 	SnakePanel panel;
-	SnakeModel model; 
+	SnakeModel model;
+	
 	SnakeFrame()
 	{
 		panel = new SnakePanel(this);
-		model = new SnakeModel(25);
+		model = new SnakeModel(); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		add(panel);
 		pack();//autosize the frame depending on component sizes
 		setLocationRelativeTo(null);
-		//addKeyListener(this);
 		setVisible(true);
 	}
 
@@ -23,30 +22,5 @@ public class SnakeFrame extends JFrame //implements KeyListener
 	{
 		return model;
 	}
-	
-	
-	/*
-	@Override
-	public void keyPressed(KeyEvent e)
-	{
-		int arrowpressed = e.getKeyCode();
-		if(arrowpressed == 37) {
-			model.moveLeft(model.getBoard());
-		}
-		if(arrowpressed == 38) {
-			model.moveUp(model.getBoard());
-		}
-		if(arrowpressed == 39) {
-			model.moveRight(model.getBoard());
-		}
-		if(arrowpressed == 40) {
-			model.moveDown(model.getBoard());
-		}
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e){}
-	@Override
-	public void keyReleased(KeyEvent e){}
-*/
 }
+
